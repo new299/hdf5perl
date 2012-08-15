70835e2473a68012cb0b4e1a86f60ca33819128b
new299 <new299@github.com>
2012-08-14 18:37:55 -0700

    defines and open

--- example.pl
+++ example.pl
@@ -5,6 +5,6 @@ use lib './blib/arch/auto/Hdf5';
 use Hdf5;
 
 #$res = Hdf5::H5Fcreate("./test.hdf5",0,0,0);
-
-$res = Hdf5::H5Fopen("./abit.fast5",0,0);
-#$res = H5Fopen("./abit.hdf5",H5F_ACC_RDONLY,H5P_DEFAULT);
+print $Hdf5::H5F_ACC_RDWR;
+#$res = Hdf5::H5Fopen("./abit.fast5",0,0);
+$res = Hdf5::H5Fopen("./abit.fast5",$Hdf5::H5F_ACC_RDONLY,$Hdf5::H5P_DEFAULT);
