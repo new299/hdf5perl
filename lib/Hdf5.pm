dde0be0a1405998e197b0bb8fab60b1f9d9c951d
Nava Whiteford <new@sgenomics.co.uk>
2013-02-06 12:51:43 +0000

    updated metadata

--- lib/Hdf5.pm
+++ lib/Hdf5.pm
@@ -77,7 +77,7 @@ __END__
 
 =head1 NAME
 
-Hdf5 - Perl extension for blah blah blah
+Hdf5 - Hdf5 reader perl extension
 
 =head1 SYNOPSIS
 
@@ -86,11 +86,7 @@ Hdf5 - Perl extension for blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for Hdf5, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+A read only HDF5 file reader.
 
 =head2 EXPORT
 
@@ -100,22 +96,15 @@ None by default.
 
 =head1 SEE ALSO
 
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
+Nothing.
 
 =head1 AUTHOR
 
-U-user-PC\user, E<lt>user@x-ray.atE<gt>
+new@sgenomics.co.uk
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2012 by U-user-PC\user
+Copyright (C) 2012 by Nava Whiteford
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
