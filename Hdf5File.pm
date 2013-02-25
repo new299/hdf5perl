3bf66205c051d8112febf109524d0a105f808d3f
cjw85 <chris.wright@nanoporetech.com>
2013-02-25 15:48:37 +0000

    clean up

--- Hdf5File.pm
+++ Hdf5File.pm
@@ -332,18 +332,10 @@ sub read_dataset_compound {
 
   my %result_data;
   for($n=0;$n<($#names)+1;$n++) {
-
-
     my $slice = HdfHelper::get_every_nth(\@as_array, scalar @names ,$n);
     $result_data{$names[$n]} = $slice;
   }
 
-  #for($n=0;$n<($#as_array)+1;$n++) {
-  #  my $i = $n % $member_count;
-  #  push($result_data{$names[$i]},$as_array[$n]);
-  #}
-
-  print "This bit is really slow\n";
   return %result_data;
 }
 
