2ac5b9abac73cd2b3bcd19e5b1380b447a11d055
cwright <chris.wright@nanoporetech.com>
2013-03-05 18:51:13 +0000

    More tests and docs

--- lib/HdfHelper/lib/HdfHelper.pm
+++ lib/HdfHelper/lib/HdfHelper.pm
@@ -34,49 +34,34 @@ XSLoader::load('HdfHelper', $VERSION);
 
 1;
 __END__
-# Below is stub documentation for your module. You'd better edit it!
 
 =head1 NAME
 
-HdfHelper - Perl extension for blah blah blah
+HdfHelper - Just some functions to make hdf5perl a bit faster
 
 =head1 SYNOPSIS
 
   use HdfHelper;
-  blah blah blah
 
 =head1 DESCRIPTION
 
-Stub documentation for HdfHelper, created by h2xs. It looks like the
-author of the extension was negligent enough to leave the stub
-unedited.
-
-Blah blah blah.
+Provides get_every_nth to get every nth element of a perl array,
+and rescale_array to perform the transformation A_i -> (A_i + a) * b
+to a perl array.
 
 =head2 EXPORT
 
 None by default.
 
-
-
 =head1 SEE ALSO
-
-Mention other useful documentation such as the documentation of
-related modules or operating system documentation (such as man pages
-in UNIX), or any relevant external documentation such as RFCs or
-standards.
-
-If you have a mailing list set up for your module, mention it here.
-
-If you have a web site set up for your module, mention it here.
 
 =head1 AUTHOR
 
-A. U. Thor, E<lt>cwright@E<gt>
+C. J. Wright, E<lt>chris.wright@nanoporetech.com<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2013 by A. U. Thor
+Copyright (C) 2013 by C. J. Wright
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.14.2 or,
--- lib/HdfHelper/t/HdfHelper.t
+++ lib/HdfHelper/t/HdfHelper.t
@@ -1,16 +1,25 @@
-# Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl HdfHelper.t'
-
-
 use strict;
 use warnings;
 
-use Test::More tests => 2;
-BEGIN { use_ok('HdfHelper') };
+use Test::More tests => 5;
+use_ok('HdfHelper');
 
-#########################
 
 my @array = (0,1,2,3,4,5,6,7,8,9);
 my $slice = HdfHelper::get_every_nth(\@array, 2, 0);
 my $expected = [0,2,4,6,8];
 is_deeply($slice, $expected, "get_every_nth");
+
+$expected = [0,1,2,3,4,5,6,7,8,9];
+HdfHelper::rescale_array(\@array, 0.0, 1.0);
+is_deeply(\@array, $expected, "rescale: do nothing");
+
+$expected = [0,2,4,6,8,10,12,14,16,18];
+HdfHelper::rescale_array(\@array, 0.0, 2.0);
+is_deeply(\@array, $expected, "rescale: double");
+
+$expected = [1,3,5,7,9,11,13,15,17,19];
+HdfHelper::rescale_array(\@array, 1.0, 1.0);
+is_deeply(\@array, $expected, "rescale: add one");
+
+
