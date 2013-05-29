dda09eef87e48e98ea0e542c130c088c6ca559ec
cjw85 <chris.wright@nanoporetech.com>
2013-05-29 12:19:29 +0200

    Bump version to 0.07

--- lib/Hdf5.pm
+++ lib/Hdf5.pm
@@ -16,7 +16,7 @@ use Readonly;
 our %EXPORT_TAGS = ( 'all' => [ qw() ] );
 our @EXPORT_OK   = ( @{ $EXPORT_TAGS{'all'} } );
 
-our $VERSION     = '0.06';
+our $VERSION     = '0.07';
 
 sub AUTOLOAD { ## no critic (ProhibitAutoloading)
   # This AUTOLOAD is used to 'autoload' constants from the constant()
